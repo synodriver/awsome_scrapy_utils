6b269a6004e4b26dd7f32a17e9406cf88c926748
synodriver <624805065@qq.com>
2020-09-25 23:30:06 +0800

    add Jsonpipeline

--- pipelines/json.py
+++ pipelines/json.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
+import scrapy
+from scrapy.exporters import JsonLinesItemExporter
 
-def main():
-    pass
 
+class JsonPipeline:
+    """
+    保存为json文件的pipeline
+    需要添加settings.py
+    JSON_PATH
+    """
 
-if __name__ == "__main__":
-    main()
+    def __init__(self, path: str):
+        self.path = path
+        self.file = None
+        self.exporter: JsonLinesItemExporter = None
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        self = cls(crawler.settings.get("JSON_PATH"))
+        return self
+
+    def open_spider(self, spider: scrapy.Spider):
+        self.file = open(self.path, "wb+")
+        self.exporter = JsonLinesItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider: scrapy.Spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
