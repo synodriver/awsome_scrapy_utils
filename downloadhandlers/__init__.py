40016a2476ec816d9b126ab6f257b6e2d5390291
synodriver <624805065@qq.com>
2021-05-20 02:29:18 +0800

    :rocket: http2 download handler using httpx

---
+++ downloadhandlers/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from .http2 import HttpxDownloadHandler
--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from .aria2 import Aria2Pipeline
 from .json import JsonPipeline
-from .mongodb import MongoDBPipeline
+from .mongodb import AsyncMongoDBPipeline, DeferredMongoDBPipeline
 from .sql import SqlPipeline
 from .text import TextPipeline
 
-__all__ = ["Aria2Pipeline", "JsonPipeline", "MongoDBPipeline", "SqlPipeline", "TextPipeline"]
+__all__ = ["Aria2Pipeline", "JsonPipeline", "AsyncMongoDBPipeline", "DeferredMongoDBPipeline", "SqlPipeline",
+           "TextPipeline"]
 __author__ = "synodriver"
